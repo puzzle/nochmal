2479167826cd27328abe884e6c74b34bf5e506ba
Matthias Viehweger <viehweger@puzzle.ch>
2022-03-22 14:43:31 +0100

    Output needed changes for carrierwave-migration

--- lib/nochmal/adapters/carrierwave_migration.rb
+++ lib/nochmal/adapters/carrierwave_migration.rb
@@ -57,12 +57,61 @@ module Nochmal
       private
 
       def carrierwave_change(model, type, uploader)
+        <<~TEXT
+          # replace #{model.name.underscore}.#{type}_url in your views
+          # Change carrierwave-uploader in #{model.name}:
+            mount_uploader :#{migration_method(type)}, #{uploader.name}, mount_on: '#{type}'
+        TEXT
       end
 
       def active_storage_change(type, uploader)
+        versions = uploader.versions.map do |name, version|
+          "attachable.variant :#{name}, #{version.processors.flat_map(&:compact)}"
+        end
+
+        return "  has_one_attached :#{type}" if versions.none?
+
+        <<~TEXT
+            has_one_attached :#{type} do |attachable|
+              #{versions.join}
+            end
+          # uploader #{type} has #{versions.size} versions
+        TEXT
       end
 
       def display_helper_notes
+        <<~RUBY
+          module ImageDisplayHelper
+            # Usage: image_tag(image_url(person, picture, '72x72'))
+            def image_url(model, name, size = nil) # rubocop:disable Metrics/MethodLength
+              if model.send(name.to_sym).attached?
+                model.send(name.to_sym).yield_self do |pic|
+                  if size
+                    # variant passes to mini_magick or vips, I assume mini_magick here
+                    pic.variant(resize_to_limit: [size, size])
+                  else
+                    pic
+                  end
+                end
+              elsif model.send(:"carrierwave_\#{name}")
+                if size
+                  model.send(:"carrierwave_\#{name}_url")
+                else
+                  model.send(:"carrierwave_\#{name}_url", size: "\#{size}x\#{size}")
+                end
+              end
+            end
+
+            # Usage: image_tag(image_variant(person, picture, :thumb))
+            def image_variant(model, name, variant)
+              if model.send(name.to_sym).attached?
+                model.send(name.to_sym).variant(variant.to_sym)
+              else
+                model.send(:"carrierwave_\#{name}").send(variant.to_sym).url
+              end
+            end
+          end
+        RUBY
       end
 
       def migration_method(type)
